89940bb73b60ef551db62d0e7d631b1567efa520
davetayls <dave@the-taylors.org>
2019-07-06 08:13:00 +0100

    removed jQuery and fixed css on post header

--- src/index.ts
+++ src/index.ts
@@ -1,100 +1,103 @@
-/**
- * Pogo Kid
- */
 const fastclick = require('fastclick')
 const debounce = require('lodash.debounce')
-import $ = require('jquery');
 const tocbot = require('tocbot')
 
 require('waypoints/lib/noframework.waypoints')
 require('waypoints/lib/shortcuts/sticky')
 
 fastclick.attach(document.body);
-(<any>window).jQuery = $
 
 const Waypoint = (<any>window).Waypoint
 
-const $pageHeader = $('.PageHeader')
+const pageHeader = document.querySelector('.PageHeader')
 const PAGE_HEADER_COLLAPSED_CLASS = 'PageHeader--collapsed'
+const PAGE_HEADER_OPEN = 'PageHeader--open'
 
-const $aboutMeAside = $('.AboutMeAside')
+const aboutMeAside = document.querySelector('.AboutMeAside')
 const ABOUT_ME_ASIDE_STICKY = 'AboutMeAside--sticky'
 const ABOUT_ME_ASIDE_BOTTOM_CLASS = 'AboutMeAside--bottom'
 
-const $workWithMeFooter = $('.WorkWithMeFooter')
+const workWithMeFooter = document.querySelector('.WorkWithMeFooter')
 
-$('pre>code').each(function (i, block) {
-  (window as any).hljs.highlightBlock(block)
-})
+document.querySelectorAll('pre>code')
+  .forEach((block) => {
+    (window as any).hljs.highlightBlock(block)
+  })
 
-$(window).on('resize', debounce(function () {
-  Waypoint.refreshAll()
-}, 40))
+window.addEventListener(
+  'resize',
+  debounce(() => {
+    Waypoint.refreshAll()
+  }, 20)
+)
 
-const $pageHeaderBurger = $pageHeader.find('.Burger')
-const PAGE_HEADER_OPEN = 'PageHeader--open'
-const BURGER_X = 'Burger--x'
-
-$pageHeaderBurger.on('click', function () {
-  $pageHeader.toggleClass(PAGE_HEADER_OPEN)
-  $pageHeaderBurger.toggleClass(BURGER_X)
-})
-
-new Waypoint({
-  element: document.getElementById('content'),
-  offset: 90,
-  handler: function (direction: any) {
-    switch (direction) {
-      case 'up':
-        $pageHeader.removeClass(PAGE_HEADER_COLLAPSED_CLASS)
-        break
-      case 'down':
-        $pageHeader.addClass(PAGE_HEADER_COLLAPSED_CLASS)
-        break
-    }
+if (pageHeader) {
+  const pageHeaderBurger = pageHeader.querySelector('.Burger')
+  const BURGER_X = 'Burger--x'
+  if (pageHeaderBurger) {
+    pageHeaderBurger.addEventListener('click', function () {
+      pageHeader.classList.toggle(PAGE_HEADER_OPEN)
+      pageHeaderBurger.classList.toggle(BURGER_X)
+    })
   }
-})
-
-if ($aboutMeAside.length) {
   new Waypoint({
-    element: $aboutMeAside[0],
-    offset: 110,
+    element: document.getElementById('content'),
+    offset: 90,
     handler: function (direction: any) {
       switch (direction) {
         case 'up':
-          $aboutMeAside.removeClass(ABOUT_ME_ASIDE_STICKY)
+          pageHeader.classList.remove(PAGE_HEADER_COLLAPSED_CLASS)
           break
         case 'down':
-          $aboutMeAside.addClass(ABOUT_ME_ASIDE_STICKY)
+          pageHeader.classList.add(PAGE_HEADER_COLLAPSED_CLASS)
           break
       }
     }
   })
+
+  if (aboutMeAside) {
+    new Waypoint({
+      element: aboutMeAside,
+      offset: 110,
+      handler: function (direction: any) {
+        switch (direction) {
+          case 'up':
+            aboutMeAside.classList.remove(ABOUT_ME_ASIDE_STICKY)
+            break
+          case 'down':
+            aboutMeAside.classList.add(ABOUT_ME_ASIDE_STICKY)
+            break
+        }
+      }
+    })
+  }
+
+
+  if (workWithMeFooter && aboutMeAside) {
+    new Waypoint({
+      element: workWithMeFooter,
+      offset: '100%',
+      handler: function (direction: any) {
+        switch (direction) {
+          case 'up':
+            aboutMeAside.classList.remove(ABOUT_ME_ASIDE_BOTTOM_CLASS)
+            break
+          case 'down':
+            aboutMeAside.classList.add(ABOUT_ME_ASIDE_BOTTOM_CLASS)
+            break
+        }
+      }
+    })
+  }
 }
 
-
-if ($workWithMeFooter.length) {
-  new Waypoint({
-    element: $workWithMeFooter[0],
-    offset: '100%',
-    handler: function (direction: any) {
-      switch (direction) {
-        case 'up':
-          $aboutMeAside.removeClass(ABOUT_ME_ASIDE_BOTTOM_CLASS)
-          break
-        case 'down':
-          $aboutMeAside.addClass(ABOUT_ME_ASIDE_BOTTOM_CLASS)
-          break
-      }
-    }
+if (document.querySelector('.js-toc')) {
+  tocbot.init({
+    // Where to render the table of contents.
+    tocSelector: '.js-toc',
+    // Where to grab the headings to build the table of contents.
+    contentSelector: '.js-toc-content',
+    // Which headings to grab inside of the contentSelector element.
+    headingSelector: 'h2, h3',
   })
 }
-
-tocbot.init({
-  // Where to render the table of contents.
-  tocSelector: '.js-toc',
-  // Where to grab the headings to build the table of contents.
-  contentSelector: '.js-toc-content',
-  // Which headings to grab inside of the contentSelector element.
-  headingSelector: 'h2, h3',
-});
